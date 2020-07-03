1be7bbab57423206f049ec6a4d6b330d25e09270
aelf-hz <mason.huang@aelf.io>
2020-07-03 11:35:27 +0800

    name

--- app/assets/theme/colors.js
+++ app/assets/theme/colors.js
@@ -10,7 +10,7 @@ const Colors = {
     fontGray: '#999999',   //字体灰
     fontColor: "#817AFD",   //颜色字体
 
-    bgColor: '#fff',    //页面背景颜色
+    bgColor: '#ffffff',    //页面背景颜色
 
     btnDefault: "",
     btnColor: "#8FC323", //绿色按钮
